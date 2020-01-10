f9b0aba725f9b1fa34e76f4515d97abef65ce9d2
Juozas Rastenis <jra@novicell.dk>
2020-01-10 13:45:30 +0100

    forgot build

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var nvcl_button_1 = __importDefault(require("./nvcl-button"));
+const nvcl_button_1 = __importDefault(require("./nvcl-button"));
 exports.NovicellButton = nvcl_button_1.default;
-var test_button_1 = __importDefault(require("./test-button"));
+const test_button_1 = __importDefault(require("./test-button"));
 exports.TestButton = test_button_1.default;
--- dist/nvcl-button.js
+++ dist/nvcl-button.js
@@ -2,56 +2,43 @@
 // Standard JS WebComponent Starter - kit
 // See README for more information
 /* eslint-disable */
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 Object.defineProperty(exports, "__esModule", { value: true });
-var StandardJSWebComponent = /** @class */ (function (_super) {
-    __extends(StandardJSWebComponent, _super);
-    function StandardJSWebComponent() {
-        var _this = 
+class StandardJSWebComponent extends HTMLElement {
+    constructor() {
         // If you define a constructor, always call super() first!
         // This is specific to CE and required by the spec.
-        _super.call(this) || this;
-        var shadow = _this.attachShadow({ mode: "open" });
-        return _this;
+        super();
+        const shadow = this.attachShadow({ mode: "open" });
     }
-    Object.defineProperty(StandardJSWebComponent, "observedAttributes", {
-        // Specify observed attributes names to be notified in attributeChangedCallback
-        get: function () {
-            return [];
-        },
-        enumerable: true,
-        configurable: true
-    });
+    // Specify observed attributes names to be notified in attributeChangedCallback
+    static get observedAttributes() {
+        return [];
+    }
     // Called when an observed attribute has been added, removed, updated, or replaced.
     // Also called for initial values when an element is created by the parser, or upgraded.
     // Note: only attributes listed in the observedAttributes property will receive this callback.
-    StandardJSWebComponent.prototype.attributeChangedCallback = function (attr, oldValue, newValue) { };
+    attributeChangedCallback(attr, oldValue, newValue) { }
     // Called every time the element is inserted into the DOM.
     // Useful for running setup code, such as fetching resources or rendering.Generally, you should try to delay work until this time.
-    StandardJSWebComponent.prototype.connectedCallback = function () {
+    connectedCallback() {
         if (!this.shadowRoot)
             return;
-        this.shadowRoot.innerHTML = "\n    <!-- scoped styles -->\n    <style>\n    .btn {\n\t\t  font-size: 150%;\n\t  }\n    </style>\n    <!-- html -->\n    <button class=\"btn\">This is a button for " + (this.getAttribute("name") ||
-            "World") + "!</button>";
-    };
+        this.shadowRoot.innerHTML = `
+    <!-- scoped styles -->
+    <style>
+    .btn {
+		  font-size: 150%;
+	  }
+    </style>
+    <!-- html -->
+    <button class="btn">This is a button for ${this.getAttribute("name") ||
+            "World"}!</button>`;
+    }
     // Called every time the element is removed from the DOM.
     // Useful for running clean up code.
-    StandardJSWebComponent.prototype.disconnectedCallback = function () { };
+    disconnectedCallback() { }
     // The custom element has been moved into a new document(e.g.someone called document.adoptNode(el)).
-    StandardJSWebComponent.prototype.adoptedCallback = function () { };
-    return StandardJSWebComponent;
-}(HTMLElement));
+    adoptedCallback() { }
+}
 exports.default = StandardJSWebComponent;
 // customElements.define("nvcl-button", StandardJSWebComponent);
